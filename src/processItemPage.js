82003bd47babf99aa86e618c1070373f16e3f875
atompkins <atompkins@gmail.com>
2020-04-16 23:19:33 +0100

    Tidy up

--- src/processItemPage.js
+++ src/processItemPage.js
@@ -1,30 +1,36 @@
 const cheerio = require('cheerio');
 const { sqlWriter } = require('./sqliteWriter');
 
+const getHeading = ($) => /(.+) \(Min Level: (\d+)\)/.exec(
+  $('td[width="800"]').find('b').text(),
+);
+
+const getTheCreatures = ($) => $('.tHeader:contains(Creatures)')
+  .parent()
+  .nextAll()
+  .find('a')
+  .filter((i, e) => $(e).text() !== '')
+  .get();
+
+const getCreatureClass = ($el) => $el
+  .parent()
+  .contents()
+  .filter((j, f) => f.type === 'text')
+  .text();
+
+const getCreatureInfo = ($) => getTheCreatures($).map((el) => [
+  /creature_id=(\d+)/.exec($(el).attr('href'))[1],
+  $(el).text(),
+  /\((.*)\)/.exec(getCreatureClass($(el)))[1],
+]);
+
 function processItemPage({ config, data }) {
   const realmId = /realm_id=(\d+)/.exec(config.url)[1];
   const $ = cheerio.load(data);
-  const heading = $('td[width="800"]').find('b').text();
-  const chunks = /(.+) \(Min Level: (\d+)\)/.exec(heading);
-  const realmName = chunks[1];
-  const level = chunks[2];
-  $('.tHeader:contains(Creatures)')
-    .parent()
-    .nextAll()
-    .find('a')
-    .filter((i, e) => $(e).text() !== '')
-    .each((i, e) => {
-      const creatureId = /creature_id=(\d+)/.exec($(e).attr('href'))[1];
-      const creatureName = $(e).text();
-      const creatureClass = /\((.*)\)/.exec(
-        $(e)
-          .parent()
-          .contents()
-          .filter((j, f) => f.type === 'text')
-          .text(),
-      )[1];
-      sqlWriter([realmId, realmName, level, creatureId, creatureName, creatureClass]);
-    });
+  const [, realmName, level] = getHeading($);
+  getCreatureInfo($).forEach((aCreature) => {
+    sqlWriter([realmId, realmName, level, ...aCreature]);
+  });
 }
 
 module.exports = processItemPage;
